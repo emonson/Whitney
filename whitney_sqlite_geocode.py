99cb81110ab95489bec3dc77f9b1a2d2c4a7040c
Eric E Monson <emonson@cs.duke.edu>
2012-08-13 17:14:43 -0400

    better http error handling and multiples ranking

--- whitney_sqlite_geocode.py
+++ whitney_sqlite_geocode.py
@@ -1,10 +1,13 @@
 import os
+import sys
 import re
 import codecs
 import sqlite3
 import exceptions
 import json
 import urllib
+import httplib
+import time
 from pymongo import Connection
 
 # Make a connection to Mongo.
@@ -21,10 +24,10 @@ data_dir = '/Users/emonson/Data/ArtMarkets/Katie'
 catalogue_file = 'Whitney_ListOfCatalogues.tsv'
 catalogue_path = os.path.join(data_dir, catalogue_file)
 
-data_file = 'Smith_20120801_rev4.txt'
+data_file = 'Smith_20120801_rev5.txt'
 data_path = os.path.join(data_dir, data_file)
 
-db_name = 'Whitney_20120810.sqlite'
+db_name = 'Whitney_20120813.sqlite'
 db_path = os.path.join(data_dir, db_name)
 
 re_name = re.compile(r'^[A-Z]')
@@ -33,6 +36,7 @@ re_artwork = re.compile(r'^([0-9]{1,3})\. (.*?)$')
 
 conn = sqlite3.connect(db_path)
 c = conn.cursor()
+
 
 # WARNING: Deleting all tables!!
 c.execute("DROP TABLE IF EXISTS catalogues")
@@ -68,6 +72,7 @@ c.execute('''CREATE TABLE addresses(
 c.execute(''' CREATE TABLE yahoo_geocodes(
 	geocode_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	address_id INTEGER,
+	multiples_rank INTEGER,
 	quality INTEGER,
 	latitude REAL,
 	longitude REAL,
@@ -108,16 +113,52 @@ def str_int(s):
 		return -1
 
 def geocode_address(address_txt):
-	base_req = 'http://where.yahooapis.com/geocode?flags=J&appid=cp7E7D7i&q='
+	# Yahoo Geocode service
+	http_host = 'where.yahooapis.com'
+	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.75 Safari/537.1', 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.3'}
+	http_conn = httplib.HTTPConnection(http_host)
+	query_dict = {'appid': 'cp7E7D7i', 'flags': 'J', 'locale':'en_US'}
+	
 	# Take care of some special cases
 	if address_txt.startswith('c/o') and (',' in address_txt):
 		txt = address_txt[address_txt.find(',')+1:]
 	else:
 		txt = address_txt
-	url = base_req + urllib.quote(txt.encode('utf-8'))
-	# TODO: Really should check return code and not just .read()
-	g_reply = urllib.urlopen(url).read()
-	return g_reply
+
+	query_dict['q'] = txt.encode('utf-8')
+	query_str = urllib.urlencode(query_dict, True)
+	# print query_str
+	
+	search_url = '/geocode?' + query_str
+	# print search_url
+	# sys.stdout.flush()
+		
+	http_conn.request("GET", search_url, None, headers)
+	
+	resp = http_conn.getresponse()
+	# print "Response Status:", resp.status
+	# sys.stdout.flush()
+	
+	if resp.status == 200:
+		returned_json = resp.read()
+		http_conn.close()
+		return returned_json
+	else:
+		# Try once more...
+		time.sleep(0.25)
+		http_conn.request("GET", search_url, None, headers)
+		time.sleep(0.25)
+		resp = http_conn.getresponse()
+		
+		if resp.status == 200:
+			print '* * Returning second try response'
+			returned_json = resp.read()
+			http_conn.close()
+			return resp.read()
+	
+	returned_status = resp.status
+	http_conn.close()
+	return returned_status
 	
 # Catalogues
 print "Loading Catalogues into DB"
@@ -165,14 +206,21 @@ for ii,line in enumerate(codecs.open(data_path, 'r', 'utf-8')):
 			if geo_dict is None:
 				geo_resultset_json = geocode_address(address_text)
 				# NOTE: This will just error out if yahoo returns nothing...
-				geo_dict = json.loads(geo_resultset_json)
+				try:
+					geo_dict = json.loads(geo_resultset_json)
+				except:
+					print 'Response:', geo_resultset_json
+					sys.exit(1)
+				
 				geo_dict['address_text'] = address_text
 				# Save it in MongoDB
 				db.yahoo_geo.save(geo_dict)
 			
 			geo_resultset_dict = geo_dict['ResultSet']
 			if geo_resultset_dict['Found'] > 0:
-				for gr in geo_resultset_dict['Results']:
+				# Keeping track of the order of results since Yahoo seems to return the best first
+				for rr, gr in enumerate(geo_resultset_dict['Results']):
+					multiples_rank = rr
 					quality = None
 					latitude = None
 					longitude = None
@@ -187,7 +235,7 @@ for ii,line in enumerate(codecs.open(data_path, 'r', 'utf-8')):
 					if 'city' in gr: city = gr['city']
 					if 'state' in gr: state = gr['state']
 					if 'country' in gr: country = gr['country']
-					c.execute("INSERT INTO yahoo_geocodes (	address_id, quality, latitude, longitude, radius, city, state, country) VALUES (?,?,?,?,?,?,?,?)", (address_id, quality, longitude, latitude, radius, city, state, country))
+					c.execute("INSERT INTO yahoo_geocodes (	address_id, quality, multiples_rank, latitude, longitude, radius, city, state, country) VALUES (?,?,?,?,?,?,?,?,?)", (address_id, quality, multiples_rank, longitude, latitude, radius, city, state, country))
 		else:
 			address_id = address_ids[address_text]
 		c.execute("INSERT INTO show_artist_addresses (show_id, artist_id, address_id) VALUES (?,?,?)", (show_id, artist_id, address_id))
